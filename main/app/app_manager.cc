14de9d3ccf07eb84261e5169a220200b1e5c8e3a
john <john@china.it>
2026-05-08 18:16:56 +0800

    主界面时间改用48px Montserrat字体
    - 启用 LV_FONT_MONTSERRAT_48（ASCII字体，~50KB）
    - 主界面时间标签增大到48px，日期保持30px中文

--- main/app/app_manager.cc
+++ main/app/app_manager.cc
@@ -11,6 +11,8 @@
 
 #define TAG "AppManager"
 
+LV_FONT_DECLARE(lv_font_montserrat_48);
+
 AppManager& AppManager::GetInstance() {
     static AppManager instance;
     return instance;
@@ -111,9 +113,9 @@ void AppManager::BuildHomeScreen() {
     lv_obj_set_flex_flow(center, LV_FLEX_FLOW_COLUMN);
     lv_obj_set_flex_align(center, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
-    // Time (use text_font, not icon_font - icon fonts don't have digits)
+    // Time - use 48px Montserrat font
     home_time_label_ = lv_label_create(center);
-    lv_obj_set_style_text_font(home_time_label_, text_font, 0);
+    lv_obj_set_style_text_font(home_time_label_, &lv_font_montserrat_48, 0);
     lv_obj_set_style_text_color(home_time_label_, fg_color, 0);
     lv_label_set_text(home_time_label_, "00:00");
 
